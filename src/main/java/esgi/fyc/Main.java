e26f8e95a526d042aa53900fcc724f9a4daab608
samiraseddar <121232123+samiraseddar@users.noreply.github.com>
2025-01-01 23:54:09 +0100

    correction mvn install bug

--- src/main/java/esgi/fyc/Main.java
+++ src/main/java/esgi/fyc/Main.java
@@ -3,26 +3,29 @@ import esgi.fyc.dao.ReservationDAO;
 import esgi.fyc.dao.UserDAO;
 import esgi.fyc.entity.Concert;
 import esgi.fyc.entity.User;
+import java.util.List;
 
-public void main() {
-   System.out.println("Bienvenue dans le système de billetterie multithreadé !");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans le système de billetterie multithreadé !");
 
-   ConcertDAO concertDAO = new ConcertDAO();
-   UserDAO userDAO = new UserDAO();
-   ReservationDAO reservationDAO = new ReservationDAO();
+        ConcertDAO concertDAO = new ConcertDAO();
+        UserDAO userDAO = new UserDAO();
+        ReservationDAO reservationDAO = new ReservationDAO();
 
-   System.out.println("\n[INFO] Liste des concerts disponibles :");
-   List<Concert> concerts = concertDAO.getAllConcerts();
-   for (Concert concert : concerts) {
-      System.out.println(" - " + concert.getName() + " le " + concert.getDate() +
-              " (Billets restants : " + concert.getRemainingTickets() + ")");
-   }
+        System.out.println("\n[INFO] Liste des concerts disponibles :");
+        List<Concert> concerts = concertDAO.getAllConcerts();
+        for (Concert concert : concerts) {
+            System.out.println(" - " + concert.getName() + " le " + concert.getDate() +
+                    " (Billets restants : " + concert.getRemainingTickets() + ")");
+        }
 
-   System.out.println("\n[INFO] Liste des utilisateurs inscrits :");
-   List<User> users = userDAO.getAllUsers();
-   for (User user : users) {
-      System.out.println(" - " + user.getName() + " (" + user.getEmail() + ")");
-   }
+        System.out.println("\n[INFO] Liste des utilisateurs inscrits :");
+        List<User> users = userDAO.getAllUsers();
+        for (User user : users) {
+            System.out.println(" - " + user.getName() + " (" + user.getEmail() + ")");
+        }
 
-   System.out.println("\n[INFO] Connexion à la base de données et tests terminés !");
+        System.out.println("\n[INFO] Connexion à la base de données et tests terminés !");
+    }
 }
